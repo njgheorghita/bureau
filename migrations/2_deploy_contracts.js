8bde34abc782270e6936df099206e385238bf3ac
Ethan Bennett <ebennett19@gmail.com>
2017-10-08 16:02:20 -0600

    Deploy bureau and controller

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,15 +1,15 @@
 var Client = artifacts.require("./Client.sol");
 var Org = artifacts.require("./Org.sol");
 var Bureau = artifacts.require("./Bureau.sol");
-// var Loan = artifacts.require("./Loan.sol");
+var Loan = artifacts.require("./Loan.sol");
 var Controller = artifacts.require("./Controller.sol");
 var Ballot = artifacts.require("./Ballot.sol");
 
 module.exports = function(deployer) {
-  // deployer.deploy(Org);
-  // deployer.deploy(Client);
+  deployer.deploy(Org);
+  deployer.deploy(Client);
   deployer.deploy(Bureau);
-  // deployer.deploy(Loan);
-  // deployer.deploy(Ballot);
+  deployer.deploy(Loan);
+  deployer.deploy(Ballot);
   deployer.deploy(Controller);
 };
