ab5846f13080dafe02b86fb479abfeb71a010118
njgheorghita <nickgheorghita@gmail.com>
2017-10-09 13:56:19 -0600

    change deploy track

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,15 +1,8 @@
-var Client = artifacts.require("./Client.sol");
-var Org = artifacts.require("./Org.sol");
 var Bureau = artifacts.require("./Bureau.sol");
-// var Loan = artifacts.require("./Loan.sol");
 var Controller = artifacts.require("./Controller.sol");
-var Ballot = artifacts.require("./Ballot.sol");
 
 module.exports = function(deployer) {
-  // deployer.deploy(Org);
-  // deployer.deploy(Client);
-  deployer.deploy(Bureau);
-  // deployer.deploy(Loan);
-  // deployer.deploy(Ballot);
-  deployer.deploy(Controller);
+  deployer.deploy(Bureau).then(function() {
+    return deployer.deploy(Controller, Bureau.address)
+  });
 };
